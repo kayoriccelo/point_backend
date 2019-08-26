98e71b0160f5b20e0233c35adc4d8cc54dc678cd
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-08-26 16:50:45 -0300

    Novo ajuste para heroku;

--- ponto_backend/settings.py
+++ ponto_backend/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'u4yt*$x6+l=i*lan_jp(dqr1z$h%@r5fab&5k%8v1&z&c)q=2f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "https://apieletronicpoint.herokuapp.com",
+]
 
 # Application definition
 
