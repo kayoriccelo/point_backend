ade8a1af58ec5a20b28b315db7d49ba28139679c
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-08-27 01:54:25 -0300

    Alterado nomenclatura de ponto para point;

--- point_backend/wsgi.py
+++ point_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ponto_backend project.
+WSGI config for point_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ponto_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'point_backend.settings')
 
 application = get_wsgi_application()
